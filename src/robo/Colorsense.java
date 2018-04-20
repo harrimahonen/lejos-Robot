c511cbbb655f18d353da5de204029ea50463e6cf
Aleksi Haaksiala <Aleksi@Aleksis-MacBook-Pro.local>
2018-04-20 10:12:20 +0300

    Added colorsense class

--- src/robo/Colorsense.java
+++ src/robo/Colorsense.java
@@ -1,5 +1,32 @@
 package robo;
 
-public class Colorsense {
+import java.io.File;
 
+import lejos.hardware.Button;
+import lejos.hardware.Sound;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.robotics.Color;
+
+public class Colorsense extends Thread {
+	EV3ColorSensor cs;
+
+	public Colorsense() {
+		cs = new EV3ColorSensor(SensorPort.S3);
+	}
+
+	public void run() {
+
+		while (!Button.ESCAPE.isDown()) {
+			switch (cs.getColorID()) {
+			case Color.BLUE:
+				
+				File music = new File("blue.wav");
+				Sound.playSample(music, 100);
+				Sound.setVolume(100);
+				break;
+				
+			}
+		}
+	}
 }
