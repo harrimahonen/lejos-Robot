a9491925cd9a07d918cd95e2055f051d639d28cf
JoelMarttala <jokke.marttala@gmail.com>
2018-05-02 13:28:23 +0300

    Added new methods for angled drive.

--- src/robo/Drive.java
+++ src/robo/Drive.java
@@ -22,53 +22,53 @@ public class Drive {
 	
 	public void driveForward() { 
 	   mA.startSynchronization();
-	   this.mA.setSpeed(1000);
-	   this.mB.setSpeed(1000);
+	   this.mA.setSpeed((int)maxSpeed);
+	   this.mB.setSpeed((int)maxSpeed);
 	   mB.forward();
 	   mA.forward();
 	   mA.endSynchronization();
 	}	
 	public void driveBackward() {
 	   mA.startSynchronization();
-	   this.mA.setSpeed(1000);
-	   this.mB.setSpeed(1000);
+	   this.mA.setSpeed((int)maxSpeed);
+	   this.mB.setSpeed((int)maxSpeed);
 	   mA.backward();
 	   mB.backward();
 	   mA.endSynchronization();
 	   }
 	public void turnLeft(){
-		this.mA.setSpeed(1000);
-		this.mB.setSpeed(1000);
+		this.mA.setSpeed((int)maxSpeed);
+		this.mB.setSpeed((int)maxSpeed);
 		mA.forward();
 		mB.backward();
 	}
 	public void turnRight() {
-		this.mA.setSpeed(1000);
-		this.mB.setSpeed(1000);
+		this.mA.setSpeed((int)maxSpeed);
+		this.mB.setSpeed((int)maxSpeed);
 		mB.forward();
 		mA.backward();
 	}
 	public void turnUpleft() {
-		this.mA.setSpeed(1000);
-		this.mB.setSpeed(500);
+		this.mA.setSpeed((int)maxSpeed);
+		this.mB.setSpeed((int)maxSpeed/3);
 		mA.forward();
 		mB.forward();
 	}
 	public void turnUpright() {
-		this.mA.setSpeed(500);
-		this.mB.setSpeed(1000);
+		this.mA.setSpeed((int)maxSpeed/3);
+		this.mB.setSpeed((int)maxSpeed);
 		mA.forward();
 		mB.forward();
 	}
 	public void turnDownleft() {
-		this.mA.setSpeed(1000);
-		this.mB.setSpeed(500);
+		this.mA.setSpeed((int)maxSpeed);
+		this.mB.setSpeed((int)maxSpeed/3);
 		mA.backward();
 		mB.backward();
 	}
 	public void turnDownright() {
-		this.mA.setSpeed(500);
-		this.mB.setSpeed(1000);
+		this.mA.setSpeed((int)maxSpeed/3);
+		this.mB.setSpeed((int)maxSpeed);
 		mA.backward();
 		mB.backward();
 	}
